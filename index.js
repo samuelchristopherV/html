b23cc76d43a2c19aef76a74968aba4d4db437713
samuelchristopherV <ssam589555@gmail.com>
2024-01-12 10:01:27 +0530

    changes

---
+++ index.js
@@ -0,0 +1,530 @@
+    //let firstname="smith"
+    //let age ="23"
+    //let student="true"      
+   
+   // age = age + 1;
+   // console.log(firstname);
+   // console.log(age)
+   // console.log(student)
+
+   // ARITHMETIC EXPRESSION
+
+   //let student = 20;
+
+   //student = student * 2
+   //console.log(student)
+
+   //USER INPUT
+
+   //let username = window.prompt("what's your name");
+   //console.log(username)
+
+   //let username;
+    //document.getElementById("mybutton").onclick = function(){
+
+        //username = document.getElementById("mytext").Value;
+
+        //console.log(username);  }
+    
+   //TYPE CONVERSATION
+
+   //let age = window.prompt("How old are you?");
+   //console.log(typeof age);
+   //age = Number(age);
+   //age +=1;
+   //console.log("Happy birthday!You are",age,"years old");
+
+   //let x;
+   //let y;
+   //let z;
+          //x = Number("3.14");
+          //y = String(3.14);
+          //z = Boolean("")
+
+          //console.log(x,typeof x);
+          //console.log(y,typeof y);
+          //console.log(z,typeof z);
+
+          //const PI = 3.14567;
+          //let radius;
+          //let circumferences;
+
+          //radius = window.prompt("Enter the radius of the circle");
+          //radius = Number(radius);
+          //circumferences = 2*PI*radius;
+          //console.log(circumferences);
+
+//STRING 
+
+          //let Phonenumber = "91-63820-98370";
+          //let username = "samuels";
+          //console.log(username.length)
+
+          //let firstletter = username.charAt(0);
+          //console.log(firstletter);
+          //console.log(username.charAt(2))
+
+          //console.log(username.indexOf("l"));
+          //console.log(username.lastIndexOf("s"));
+
+          //username = username.trim();
+          //console.log(username);
+
+          //username = username.toUpperCase();
+          //console.log(username);
+
+          //username = username.toLowerCase();
+          //console.log(username);
+
+          //Phonenumber = Phonenumber.replaceAll("-","/");
+          //console.log(Phonenumber)
+
+//STRING FUNCTION   
+
+         //let fullName = "christopher samuel";
+         //let firstName;
+         //let lastName;
+
+         //firstName = fullName.slice(0,11);
+         //lastName = fullName.slice(12);
+
+         //console.log(firstName);
+         //console.log(lastName);
+
+//METHOD CHAIN
+
+         //let userName = "Samuels";
+         //let letter = userName.charAt(3).toUpperCase();
+         //console.log(letter);
+
+//C0NDITIONAL STATEMENT    
+
+        // let age = 70;
+
+        //if (age >= 60) {
+        //    console.log("you are a senior citizen");
+        // }
+         
+        // else if (age >= 18) {
+        //   console.log("You are an adult");
+        // }
+        
+
+        // else if (age < 0) {
+        //    console.log("you haven't been born yet");
+        //}
+
+        //else {
+        //    console.log("you are an child")
+        //}
+
+//CHECKED PROPERTY
+
+    // document.getElementById("myButton").onclick = function(){
+    //    if(document.getElementById("myCheckBox").checked){
+    //        console.log("you are subscribed");  }
+    //    else{console.log("you are not subscribed");}
+
+    // }
+
+    // document.getElementById("myButton").onclick = function() {
+    //    const myCheckBox = document.getElementById("myCheckBox");
+    //    const visabtn = document.getElementById("visabtn");
+    //    const mastercardbtn = document.getElementById("mastercardbtn");
+    //    const Rupaybtn = document.getElementById("Rupaybtn");
+     
+
+    // if(visabtn.checked) {
+    //    console.log("you are paying with visa");
+    // }
+
+    //  else if(mastercardbtn.checked) {
+    //    console.log("you are paying with mastercard");
+    // }
+
+    //  else if(Rupaybtn.checked) {
+    //    console.log("you are paying with Rupay");
+    // }
+
+    // else {
+    //    console.log("you haven't select the payment method")
+    // }
+
+    //}
+
+//CONTROL OR LOOP STATEMEMT
+
+   //for (let counter =1; counter <=10; counter +=1){
+   //console.log(counter)
+   //console.log("game")
+   //}
+
+//ARRAYS
+
+    //let fruits = ["Apple","Orange","Banana"]
+    // fruits[2] ="coconut";
+    //fruits.push("lemon");
+    //fruits.pop();
+    //fruits.unshift("mango");
+    //fruits.shift();
+    //console.log(fruits);
+
+    //let length = fruits.length;
+    //let index = fruits.indexOf("Orange");
+    //console.log(length);
+    //console.log(index);    
+
+//LOOP IN ARRAY
+
+    //let prices =[5,10,15,20]
+    //console.log(prices);
+
+    //for(let i of prices){    ----->adavnce loop
+    //    console.log(i);
+    //}
+    //for(let i =0; i <prices.length; i +=1) { 
+    //    console.log(prices[i]);            ----->right
+    //}
+    //for(let i =prices.length - 1; i >=0; i -=1){
+    //    console.log(prices[i]);          ----->left
+    //}
+
+//FUNCTIONS
+
+   //Startprogram();
+    //function Startprogram(){
+        //let userName ="samuel";
+        //let age ="24";
+        //happybirthday(userName,age);
+    //}
+
+     //function happybirthday(userName,age) {
+        //console.log("happy bday to you!!")
+        //console.log("happy bday to you!!")
+        //console.log("happy bday to", userName )
+        //console.log("happy bday to you!!")
+        //console.log("you are",age,"old")
+    // }
+
+//RETURN KEYWORD
+
+    //let area;
+    //let width;
+    //let height;
+    //    width = window.prompt("Enter the width");
+    //    height = window.prompt("Enter the height");
+
+    //    area = getArea(width,height);
+    //    console.log("The area is",area);
+    //    function getArea(width,height){
+    //        return width*height;
+    //    }
+
+//MAPS
+
+    //   const store = new Map([
+    //     ["T-shirt",20],
+    //     ["jeans",30],
+    //     ["shirt",40],
+    //   ])
+      
+    //  console.log(store);
+
+    //store.forEach((value,key) => console.log(`${key} ${value}`));
+
+    //let shoppingcart = 0;
+    // shoppingcart += store.get("T-shirt");
+    // console.log(shoppingcart);
+
+    // store.set("hat",50);
+    // store.delete("hat");
+    // console.log(store.has("hat"));
+
+//ARROW FUNCTION
+   
+    // function greeting(userName){
+    //    console.log(`hello ${userName}`);
+    // }
+    // greeting("samuel");
+
+    // const greeting = function(userName){
+    //    console.log(`hello ${userName}`);
+    // }
+
+    //const greeting = (userName) => console.log(`hello ${userName}`);
+
+    //greeting("samuel")
+    //const percent = function(x,y){
+    //    return x/y *100;
+    //}
+
+//OBJECTS
+
+    // const car ={
+    //    model:"Thar",
+    //    color:"Black",
+    //    year:"2022",
+
+    //    drive :function(){
+    //        console.log("you drive the car");
+    //    },
+    //    brake:function(){
+    //        console.log("you put the brake")
+    //    }}
+
+    //    console.log(car.model)
+    //    console.log(car.color)
+    //    console.log(car.year)
+    //    car.drive();
+    //    car.brake();
+
+        //const car1 ={
+        //    model:"audi",
+        //    color:"red",
+        //    year:"2032",
+    
+        //drive :function(){
+        //    console.log("you drive the car");
+        //    },
+        //   brake:function(){
+        //        console.log("you put the brake")
+        //    }}
+
+        // console.log(car1.model)
+        //console.log(car1.color)
+        //console.log(car1.year)
+        //car1.drive();
+        //car1.brake();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+         
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
